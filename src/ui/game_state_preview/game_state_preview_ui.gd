793b5475ebd47a98a7a7a318ecf7562e59974282
tory37 <tory37@gmail.com>
2026-05-26 20:15:44 -0500

    Code Review Again

--- src/ui/game_state_preview/game_state_preview_ui.gd
+++ src/ui/game_state_preview/game_state_preview_ui.gd
@@ -43,7 +43,10 @@ func _on_turn_synced(turn: int) -> void:
 func _on_player_hand_synced(player_id: int, hand: GameCardCollection) -> void:
 	var card_titles: String = ""
 	for card in hand.cards:
-		card_titles += card.data.title + ", "
+		if card is GameCard:
+			card_titles += card.data.title + ", "
+		else: 
+			card_titles += "Hidden" + ", "
 	Loggit.p("Player %d hand is: %s" % [player_id, card_titles], "Flow")
 
 
@@ -60,5 +63,8 @@ func _on_player_deck_synced(player_id: int, deck: GameCardCollection) -> void:
 func _on_player_discard_synced(player_id: int, discard: GameCardCollection) -> void:
 	var card_titles: String = ""
 	for card in discard.cards:
-		card_titles += card.data.title + ", "
+		if card is GameCard:
+			card_titles += card.data.title + ", "
+		else:
+			card_titles += "Hidden" + ", "
 	Loggit.p("Player %d discard is: %s" % [player_id, card_titles], "Flow")
